4050ab18c11f74b85c9999e688ef95f37b4bd8d5
Patrick O'Neill <pgoneill@gmail.com>
2012-01-10 14:41:32 -0500

    Added svn-diff and subl-proj.

--- .bash/functions.sh
+++ .bash/functions.sh
@@ -1,7 +1,15 @@
-parse_git_branch(){ 
+parse_git_branch(){
     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/[\1] /';
 }
 
-parse_svn_rev(){ 
+parse_svn_rev(){
     svn info 2> /dev/null | grep "Revision" | sed 's/Revision: \(.*\)/[r\1] /';
+}
+
+svn-diff(){
+    svn diff "${@}" | colordiff
+}
+
+subl-proj(){
+    subl --project ~/Documents/"${@}".sublime-project
 }
