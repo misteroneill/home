96d3d5e684a4fc95e54eba0eeafea642127cb5f4
Pat O'Neill <pgoneill@gmail.com>
2017-07-25 17:30:34 -0400

    More output and exclude osxkeychain config from Linux

--- dotfiles.sh
+++ dotfiles.sh
@@ -3,10 +3,16 @@
 SRC_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 cd ${SRC_DIR}
 
+if [ $(uname) = "Darwin" ]; then
+  IS_MAC=true
+fi
+
 link_unless_exists(){
   if [ ! -e ${HOME}/${2} ]; then
     echo "Linking $2..."
     ln -sf ${SRC_DIR}/dotfiles/${1} ${HOME}/${2}
+  else
+    echo "NOT linking $2 because it exists."
   fi
 }
 
@@ -24,7 +30,11 @@ git_global_config "color.ui"               "true"
 git_global_config "color.pager"            "true"
 git_global_config "core.excludesfile"      "~/.gitignore"
 git_global_config "core.whitespace"        "trailing-space,space-before-tab"
-git_global_config "credential.helper"      "osxkeychain"
+
+if [ $IS_MAC ]; then
+  git_global_config "credential.helper"      "osxkeychain"
+fi
+
 git_global_config "fetch.prune"            "true"
 git_global_config "push.default"           "upstream"
 git_global_config "user.name"              "Pat O'Neill"
